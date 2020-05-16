67e6bbd5c1596072ebdcdc5fe9c05bbb68d86c0f
Vaibhav Jindal <vaibhav.jindal.2001@gmail.com>
2020-05-16 18:54:34 +0530

    npm scripts and build

---
+++ js/scripts.js
@@ -0,0 +1,34 @@
+$(document).ready(function(){
+    $("#mycarousel").carousel( { interval: 2000 } );
+    /*
+    $("#carousel-pause").click(function(){
+        $("#mycarousel").carousel('pause');
+    });
+    $("#carousel-play").click(function(){
+        $("#mycarousel").carousel('cycle');
+    });
+    */
+    $("#carouselButton").click(function(){
+        if ($("#carouselButton").children("span").hasClass('fa-pause')) {
+            $("#mycarousel").carousel('pause');
+            $("#carouselButton").children("span").removeClass('fa-pause');
+            $("#carouselButton").children("span").addClass('fa-play');
+        }
+        else if ($("#carouselButton").children("span").hasClass('fa-play')){
+            $("#mycarousel").carousel('cycle');
+            $("#carouselButton").children("span").removeClass('fa-play');
+            $("#carouselButton").children("span").addClass('fa-pause');                    
+        }
+    });
+    $('[href="#reserveForm"]').click(function() {
+        $("#reserveForm").modal();
+    });
+    $('[href="#login"]').click(function() {
+        $("#loginModal").modal();
+    });
+});
+
+// Tooltip
+/*$(document).ready(function() {
+    $('[data-toggle="tooltip"]').tooltip();
+});*/
